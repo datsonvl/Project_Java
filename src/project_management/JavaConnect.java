93e3e78e7bb69cc3db1c02d748d172b01e7e8209
chontrai252 <anhtaibka252@gmail.com>
2015-10-09 22:53:21 +0700

    Class tạo kết nối với Sqlite

---
+++ src/project_management/JavaConnect.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package project_management;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Tran Dat
+ */
+public class JavaConnect {
+ 
+    public static Connection connecrDB(){
+        try{
+            Class.forName("org.sqlite.JDBC");
+            Connection conn = DriverManager.getConnection("jdbc:sqlite:E:\\SQLite\\Login.sqlite");
+            JOptionPane.showMessageDialog(null, "Connection Established");
+            return conn;
+        }catch(Exception e){
+            JOptionPane.showMessageDialog(null, e);
+            return null;
+        
+        }
+    }
+    
+}
